628d400781504b8ec0bf527ef4f55fd77dc1d712
Tiffany Chien <tiffany_chien@berkeley.edu>
2021-02-11 21:45:57 -0800

    Add function average() to pipeline.py to compute master dark, etc

--- pipeline.py
+++ pipeline.py
@@ -5,13 +5,12 @@ from matplotlib import colors
 import scipy.stats
 
 # inputs:
-	# folder: path to where the .fits files are, e.g. '2016jul14/'
-	# nums: (start, end) range of indices of images, e.g. (95,96)
-		# assumes that the filenames look like e.g. n0095.fits
+    # folder: path to where the .fits files are, e.g. '2016jul14/'
+    # start, end: range of indices of images, e.g. 95,96
+        # assumes that the filenames look like e.g. n0095.fits
 # outputs:
-	# list of full paths to images, e.g. ['2016jul14/n0095.fits', '2016jul14/n0096.fits']
-def get_file_names(folder, nums):
-    start, end = nums
+    # list of full paths to images, e.g. ['2016jul14/n0095.fits', '2016jul14/n0096.fits']
+def get_file_names(folder, start, end):
     lst = []
     for num in range(start, end+1):
         num_digits = len(str(num))
@@ -20,12 +19,18 @@ def get_file_names(folder, nums):
 
 # Opens (and closes) a list of .fits file and returns the data (numpy arrays) in the PrimaryHDUs
 # inputs:
-	# list of full paths to .fits file, e.g. output of get_file_names()
+    # list of full paths to .fits file, e.g. output of get_file_names()
 # outputs:
-	# numpy array of all data (shape: n_images x image_size x image_size)
+    # numpy array of all data (shape: n_images x image_size x image_size)
 def get_data(filenames):
     data = []
     for filename in filenames:
-	    with fits.open(filename) as hdul:
-	        data.append(hdul[0].data)
+        with fits.open(filename) as hdul:
+            data.append(hdul[0].data)
     return np.array(data)
+
+# Computes average of given frames
+def average(folder, start, end):
+    filenames = get_file_names(folder, start, end)
+    all_frames = get_data(filenames)
+    return np.mean(all_frames, axis=0)
